d0f9b1370cce6dc633731eb4e60bf8455e11c893
fdeboom <faithdeboom@gmail.com>
2023-10-26 10:38:46 -0400

    added comments

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def decoder(pw):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    menu = "Menu\n----------\n1. Encode\n2. Decode\n3. Quit\n\n"
+    menu = "Menu\n----------\n1. Encode\n2. Decode\n3. Quit\n\n"  # menu string variable
     print(menu)
     choice = 4
     password = ""
-    while choice != 3:
+    while choice != 3:  # runs until choice == 3 (exit option chosen)
         choice = int(input('Please enter an option: '))
-        if choice == 1:
+        if choice == 1:  # encodes password
             password = input('Please enter your password to encode: ')
             password = encoder(password)
             print('Your password has been encoded and stored!')
-        elif choice == 2:
+        elif choice == 2:  # decodes password
             decoded = decoder(password)
             print(f'The encoded password is {password}, and the original password is {decoded}.')
