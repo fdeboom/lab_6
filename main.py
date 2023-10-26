1b0d20b43b56b6cfb7cb0234f6fdb49b9ad53d2e
fdeboom <faithdeboom@gmail.com>
2023-10-25 20:42:06 -0400

    check

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+def encoder(password):  # encodes password by adding 3 to each digit
+    for i in len(password):
+        password[i] = str(int(password[i]) + 3)
+    return password
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    encoder("123456")
